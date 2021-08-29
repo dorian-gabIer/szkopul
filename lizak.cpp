7a08813ba5ef65d12272ee520b12e2ef08f0f0df
Dorian Gabler <dorian@euro-web.eu>
2021-08-30 00:27:34 +0200

    123

--- lizak.cpp
+++ lizak.cpp
@@ -2,9 +2,29 @@
 using namespace std;
 int main()
 {
-    int rozmiar, testy;
-    string lizak;
-    cin >> rozmiar >> testy;
-    cin >> lizak;
-    while
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int s, q, k, suma = 0;
+    string str;
+    map <int, pair<int, int>> M;
+    cin >> s >> q;
+    cin >> str;
+    for(int j = 0; j < s; j++) 
+    {
+        suma = 0;
+        for(int g = j; g < s; g++)
+        {
+            if(str[g] == 'T') suma += 2;
+            else suma++;
+            M[suma] = {j+1, g+1};
+        }
+    }
+    for(int i = 0; i < q; i++) 
+    {
+        cin >> k;
+        if(M[k].first < 1 || M[k].second < 1) cout << "NIE\n";
+        else cout << M[k].first << ' ' << M[k].second << '\n';
+    }
+    return 0;
 }
